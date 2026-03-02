382002aefcaaa5c02ac995abeb39f410efe3a3ea
Rishab Seshadri <rishab.seshadri@gmail.com>
2026-03-02 18:58:35 -0500

    ...

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -14,18 +14,92 @@ class Drivetrain:
         self.odometry = odometry
 
     def go_forward(self, duty, distance):
-        
         # loop until bot's position is within some threshold of the target position, with a timeout to prevent infinite loops
         _, y = self.odometry.get_position()
         target_y = y + distance
 
-        self.motors[0].move(duty)  # front left
-        self.motors[1].move(-duty)  # front right
-        self.motors[2].move(duty)  # back left
-        self.motors[3].move(-duty)  # back right
+        kP = 0.1
+        kI = 0.0
+        kD = 0.1
+
+        integral = 0
+        last_error = 0
+
+        while abs(y - target_y) > 0.1:  # 0.1 inch threshold
+            y, _ = self.odometry.get_position()
+            error = target_y - y
+
+            integral += error
+            derivative = error - last_error
+            last_error = error
+
+            output = kP*error + kI*integral + kD*derivative
+
+            self.motors[0].move(output)
+            self.motors[1].move(-output)
+            self.motors[2].move(output)
+            self.motors[3].move(-output)
+            _, y = self.odometry.get_position()
+            yield
+        
+        self.motors[0].stop()
+        self.motors[1].stop()
+        self.motors[2].stop()
+        self.motors[3].stop()
+
+    def go_backward(self, duty, distance):
+        
+        # loop until bot's position is within some threshold of the target position, with a timeout to prevent infinite loops
+        _, y = self.odometry.get_position()
+        target_y = y - distance
+
+        self.motors[0].move(-duty)  # front left
+        self.motors[1].move(duty)  # front right
+        self.motors[2].move(-duty)  # back left
+        self.motors[3].move(duty)  # back right
 
         while abs(y - target_y) > 0.1:  # 0.1 inch threshold
             _, y = self.odometry.get_position()
+            yield
+        
+        self.motors[0].stop()
+        self.motors[1].stop()
+        self.motors[2].stop()
+        self.motors[3].stop()
+
+    def strafe_left(self, duty, distance):
+        
+        # loop until bot's position is within some threshold of the target position, with a timeout to prevent infinite loops
+        x, _ = self.odometry.get_position()
+        target_x = x - distance
+
+        self.motors[0].move(duty)  # front left
+        self.motors[1].move(duty)  # front right
+        self.motors[2].move(-duty)  # back left
+        self.motors[3].move(-duty)   # back right
+
+        while abs(x - target_x) > 0.1:  # 0.1 inch threshold
+            x, _ = self.odometry.get_position()
+            yield
+        
+        self.motors[0].stop()
+        self.motors[1].stop()
+        self.motors[2].stop()
+        self.motors[3].stop()
+
+    def strafe_right(self, duty, distance):
+        
+        # loop until bot's position is within some threshold of the target position, with a timeout to prevent infinite loops
+        x, _ = self.odometry.get_position()
+        target_x = x + distance
+
+        self.motors[0].move(-duty)  # front left
+        self.motors[1].move(-duty)  # front right
+        self.motors[2].move(duty)  # back left
+        self.motors[3].move(duty)   # back right
+
+        while abs(x - target_x) > 0.1:  # 0.1 inch threshold
+            x, _ = self.odometry.get_position()
             yield
         
         self.motors[0].stop()
