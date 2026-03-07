0673e5d5d9b7eb3bd748af3c8eb92816ea7b0718
Rishab Seshadri <rishab.seshadri@gmail.com>
2026-03-07 16:43:07 -0500

    move while holding infra

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -2,24 +2,31 @@ import time
 
 from utils.motor import Motor
 from subsystems.odometry import Odometry
+from subsystems.intake import Intake
 
 class Drivetrain:
     SLIP            = 0.1              # Adjust value for testing
     TIME_PER_PI     = 4.11             # Time in seconds
 
-    def __init__(self, motors: list[Motor], odometry: Odometry = None):
+    def __init__(self, motors: list[Motor], odometry: Odometry = None, intake: Intake = None):
         # in order of front left, front right, back left, back right
         self.motors = motors
         self.odometry = odometry
+        self.intake = intake
 
-    def go_forward(self, distance):
+    def go_forward(self, distance, holding=False):
         _, y, _ = self.odometry.get_position()
         target_y = y + distance
+
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
 
         self.motors[0].move(15)
         self.motors[1].move(-15)
         self.motors[2].move(15)
         self.motors[3].move(-15)
+
 
         while abs(target_y - y) > self.SLIP:
             _, y, _ = self.odometry.get_position()
@@ -32,29 +39,37 @@ class Drivetrain:
         self.motors[3].stop()
 
 
-    def go_backward(self, distance):
+    def go_backward(self, distance, holding=False):
         _, y, _ = self.odometry.get_position()
         target_y = y - distance
+
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
 
         self.motors[0].move(-15)
         self.motors[1].move(15)
         self.motors[2].move(-15)
         self.motors[3].move(15)
 
+
         while abs(y - target_y) > self.SLIP:
             _, y, _ = self.odometry.get_position()
             print(abs(y-target_y))
-            yield        
+            yield
 
         self.motors[0].stop()
         self.motors[1].stop()
         self.motors[2].stop()
         self.motors[3].stop()
 
-    def strafe_left(self, distance):
-        
+    def strafe_left(self, distance, holding=False):
         x, _, _ = self.odometry.get_position()
         target_x = x - distance
+        
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
 
         self.motors[0].move(15)
         self.motors[1].move(15)
@@ -71,10 +86,13 @@ class Drivetrain:
         self.motors[2].stop()
         self.motors[3].stop()
 
-    def strafe_right(self, distance):
-        
+    def strafe_right(self, distance, holding=False):
         x, _, _ = self.odometry.get_position()
         target_x = x + distance
+
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
 
         self.motors[0].move(-15)
         self.motors[1].move(-15)
@@ -91,10 +109,14 @@ class Drivetrain:
         self.motors[2].stop()
         self.motors[3].stop()
 
-    def turn_left(self, rotation):
+    def turn_left(self, rotation, holding=False):
 
         duration = rotation * self.TIME_PER_PI
-        
+
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
+
         self.motors[0].move(-15)
         self.motors[1].move(-15)
         self.motors[2].move(-15)
@@ -112,9 +134,13 @@ class Drivetrain:
         self.motors[3].stop()
 
 
-    def turn_right(self, rotation):
+    def turn_right(self, rotation, holding=False):
         
         duration = rotation * self.TIME_PER_PI
+
+        if holding:
+            self.intake.main_servo.move(-0.5)
+            self.intake.lift_servo.set_angle(100)
         
         self.motors[0].move(15)
         self.motors[1].move(15)
