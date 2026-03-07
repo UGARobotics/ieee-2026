1dac3ae30525b3a6fd00c36f0a7897b63a006c4f
Keira Inks <keira.tintas23@gmail.com>
2026-03-07 10:29:51 -0500

    Startup sys + WIP light sensor code

--- subsystems/startup_system.py
+++ subsystems/startup_system.py
@@ -1,7 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+from utils.light_sensor import LightSensor
+
+
+IDLE = 1 # will start off as IDLE
+RUNNING = 2 # when light is detected
+WAITING = 3 # IDLE -> WAITING when gpio input detected
 
 class StartupSystem:
     """Subsystem for the startup system"""
-    def __init__(self, pin=-1):
+    def __init__(self, pin=-1): # dummy pin num rn
         self.pin = pin
-    
-        # when on, we have to wait for light sensor to detect light before we can start the bot. 
+        self._command_state = StartupSystem.IDLE # starts as IDLE
+
+        GPIO.setmode(GPIO.BCM) # GPIO num instead of actual pin num
+        GPIO.setup(self.pin, GPIO.IN) # waiting for input
+        
+        self.light_sensor = LightSensor(pin)    
+
+    def _set_command_set(self, state):
+        self._set_command_state = state
+
+    def _is_high(self) -> bool:
+        if GPIO.HIGH == 1 and self._set_command_state == StartupSystem.IDLE:
+            return True
+        else:
+            return False
+
+    def update(self):
+        if self._is_high(): # is high + idle -> waiting
+            self._command_state = StartupSystem.WAITING
+
+        elif self._command_state == StartupSystem.WAITING:
+            pass
+            # needs to check for status of light here
+            # self.light_sensor.update()
+            # if light_sensor detects light: WAITING -> RUNNING
+
+    def stop(self):
+        self._set_command_state(StartupSystem.IDLE) # idk
+        GPIO.cleanup(self.pin)
--- utils/light_sensor.py
+++ utils/light_sensor.py
@@ -1,1 +1,25 @@
-# TODO: Write this
+import RPi.GPIO as GPIO
+import time
+
+DETECTED = 0 # light detected
+UNDETECTED = 1 # no light detected
+
+class LightSensor:
+    """
+    Subsystem for light sensor - wip since not hooked up
+    """
+    def __init__(
+            self, 
+            pin,
+    ):
+        
+        self.pin = pin
+        self.state = None
+
+    
+    def stop(self):
+      pass
+
+    
+    def update(self): 
+        pass        
